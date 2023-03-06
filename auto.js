0fdd5cf1e474b04d4355167f4a2e4f7f3905a080
valeriiaoshchepkova <valeriaosepkova78@gmail.com>
2023-03-06 20:22:19 +0100

    Create auto.js

---
+++ auto.js
@@ -0,0 +1,5 @@
+const orderedQuantity = 6;
+const pricePerDroid = 800;
+const deliveryFee = 50;
+const totalPrice = (pricePerDroid*orderedQuantity+deliveryFee);
+const message = "You ordered droids worth ${totalPrice} credits.Delivery ( ${deliveryFee} credits) is included in total price."
